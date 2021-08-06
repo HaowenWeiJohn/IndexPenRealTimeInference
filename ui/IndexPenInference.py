308a8b64937a415fd853658a76c7f6c37e4ba787
HaowenWeiJohn <hwei@wpi.edu>
2021-08-06 11:54:01 -0400

    indexpen realtime inference GUI finish

--- ui/IndexPenInference.py
+++ ui/IndexPenInference.py
@@ -11,6 +11,21 @@ from utils.ui_utils import *
 from config import config_path
 from utils.sound import *
 
+import datetime
+import glob
+import pathlib
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import CSVLogger
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+import time
+
+import sys
+
 
 class IndexPenInference(QtWidgets.QWidget):
     def __init__(self, parent):
@@ -20,21 +35,20 @@ class IndexPenInference(QtWidgets.QWidget):
 
         self.ui = uic.loadUi("ui/IndexPenInference.ui", self)
 
-
         # Left objectName: indexpeninference_display_Widget layoutName: indexpeninference_display_vertical_layout
         self.indexpeninference_display_container, self.indexpeninference_display_layout = init_container \
             (parent=self.indexpeninference_display_vertical_layout, vertical=True, label='IndexPen Real Time Inference')
-
 
         self.most_recent_detection_label = QLabel(text='Most Recent Detection:')
         self.indexpeninference_display_layout.addWidget(self.most_recent_detection_label)
         self.most_recent_detection_label.setAlignment(QtCore.Qt.AlignCenter)
         self.most_recent_detection_label.adjustSize()
 
-        self.indexpeninference_text_layout, self.indexpeninference_text_input = init_textEditInputBox(parent=self.indexpeninference_display_layout,
-                                                                      label='IndexPen Inference Text Input :',
-                                                                      default_input=config_ui.indexPen_classes_default,
-                                                                      vertical=True)
+        self.indexpeninference_text_layout, self.indexpeninference_text_input = init_textEditInputBox(
+            parent=self.indexpeninference_display_layout,
+            label='IndexPen Inference Text Input :',
+            default_input=config_ui.indexPen_classes_default,
+            vertical=True)
 
         # plotting container
         self.indexpeninference_plot_container, self.indexpeninference_plot_layout = init_container \
@@ -47,36 +61,40 @@ class IndexPenInference(QtWidgets.QWidget):
         plot_widget = pg.PlotWidget()
         self.indexpeninference_plot_layout.addWidget(plot_widget)
 
-
-###########################################################
+        ###########################################################
         # Right objectName: indexpeninference_control_Widget layoutName: indexpeninference_control_vertical_layout
         # control pannel vertical layout
         self.indexpeninference_control_container, self.indexpeninference_control_layout = init_container \
-            (parent=self.indexpeninference_control_vertical_layout, vertical=True, label='IndexPen Inference LSL Connection Control Panel')
+            (parent=self.indexpeninference_control_vertical_layout, vertical=True,
+             label='IndexPen Inference LSL Connection Control Panel')
         # check box hide realtime inference plot
 
         self.indexpeninference_lslconnection_control_container, self.indexpeninference_lslconnection_control_layout = init_container \
             (parent=self.indexpeninference_control_layout, vertical=True, label='IndexPen Inference LSL control')
-        self.indexpeninference_lslname_layout, self.indexpeninference_lslname_input = init_inputBox(parent=self.indexpeninference_lslconnection_control_layout,
-                                                                      label='IndexPen mmWave LSL Outlet Name :',
-                                                                      default_input=config_ui.mmWave_lsl_outlet_name_default)
-        self.connect_mmwave_lsl_btn = init_button(parent=self.indexpeninference_lslconnection_control_layout, label='Connect mmWave LSL')
+        self.indexpeninference_lslname_layout, self.indexpeninference_lslname_input = init_inputBox(
+            parent=self.indexpeninference_lslconnection_control_layout,
+            label='IndexPen mmWave LSL Outlet Name :',
+            default_input=config_ui.mmWave_lsl_outlet_name_default)
+        self.connect_mmwave_lsl_btn = init_button(parent=self.indexpeninference_lslconnection_control_layout,
+                                                  label='Connect mmWave LSL')
 
         self.indexpeninference_modelselection_container, self.indexpeninference_modelselection_layout = init_container \
             (parent=self.indexpeninference_control_layout, vertical=True, label='IndexPen Inference Model')
-        self.indexpeninference_modelpath_layout, self.indexpeninference_modelpath_input = init_inputBox(parent=self.indexpeninference_modelselection_layout,
-                                                                      label='IndexPen Model Path :',
-                                                                      default_input=config_ui.mmWave_lsl_outlet_name_default)
-        self.load_indexpen_model_btn = init_button(parent=self.indexpeninference_modelselection_layout, label='Load IndexPen Model')
-
+        self.indexpeninference_modelpath_layout, self.indexpeninference_modelpath_input = init_inputBox(
+            parent=self.indexpeninference_modelselection_layout,
+            label='IndexPen Model Path :',
+            default_input=config_ui.mmWave_lsl_outlet_name_default)
+        self.load_indexpen_model_btn = init_button(parent=self.indexpeninference_modelselection_layout,
+                                                   label='Load IndexPen Model')
 
         self.indexpeninference_plot_checkbox, self.indexpeninference_plot_checkbox = init_checkBox(
             parent=self.indexpeninference_control_layout, label='Inference Plot Hidden: ', default_checked=False)
 
-
         # button clicked
         self.load_indexpen_model_btn.clicked.connect(self.load_indexpen_model_btn_clicked)
         self.connect_mmwave_lsl_btn.clicked.connect(self.connect_mmwave_lsl_btn_clicked)
+
+        self.interpreter_quant = None
 
     def load_indexpen_model_btn_clicked(self):
         print('load_indexpen_model_btn clicked')
@@ -85,7 +103,13 @@ class IndexPenInference(QtWidgets.QWidget):
     def connect_mmwave_lsl_btn_clicked(self):
         print('connect_mmwave_lsl_btn clicked')
 
+        self.interpreter_quant = tf.lite.Interpreter(model_path=str())
+        self.interpreter_quant.allocate_tensors()
+
+        # interpreter.set_tensor(input1_index, np.expand_dims(np.array(X_mmw_rD_test[0]), axis=0).astype(np.float32))
+        # interpreter.set_tensor(input2_index, np.expand_dims(np.array(X_mmw_rA_test[0]), axis=0).astype(np.float32))
+        #
+        # interpreter.invoke()
+        # predictions = interpreter.get_tensor(output_index)
+
         return
-
-
-
