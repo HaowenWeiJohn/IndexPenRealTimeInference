933ec5492ca3e2d6014bbb5aad09e6a2d8c541cf
HaowenWeiJohn <hwei@wpi.edu>
2021-08-10 01:08:25 -0400

    indexpen realtime inference GUI finish

--- config/config_path.py
+++ config/config_path.py
@@ -1,3 +1,2 @@
-
-
 indexpen_gesture_image_dir = 'media/Gesture_img'
+indexpen_model_path = 'resources/model/indexpen_model/indexpen_model.tflite'
--- ui/IndexPenInference.py
+++ ui/IndexPenInference.py
@@ -69,6 +69,17 @@ class IndexPenInference(QtWidgets.QWidget):
              label='IndexPen Inference LSL Connection Control Panel')
         # check box hide realtime inference plot
 
+
+        self.indexpeninference_modelselection_container, self.indexpeninference_modelselection_layout = init_container \
+            (parent=self.indexpeninference_control_layout, vertical=True, label='IndexPen Inference Model')
+        self.indexpeninference_modelpath_layout, self.indexpeninference_modelpath_input = init_inputBox(
+            parent=self.indexpeninference_modelselection_layout,
+            label='IndexPen Model Path :',
+            default_input=config_path.indexpen_model_path)
+        self.load_indexpen_model_btn = init_button(parent=self.indexpeninference_modelselection_layout,
+                                                   label='Load IndexPen Model')
+
+
         self.indexpeninference_lslconnection_control_container, self.indexpeninference_lslconnection_control_layout = init_container \
             (parent=self.indexpeninference_control_layout, vertical=True, label='IndexPen Inference LSL control')
         self.indexpeninference_lslname_layout, self.indexpeninference_lslname_input = init_inputBox(
@@ -78,14 +89,6 @@ class IndexPenInference(QtWidgets.QWidget):
         self.connect_mmwave_lsl_btn = init_button(parent=self.indexpeninference_lslconnection_control_layout,
                                                   label='Connect mmWave LSL')
 
-        self.indexpeninference_modelselection_container, self.indexpeninference_modelselection_layout = init_container \
-            (parent=self.indexpeninference_control_layout, vertical=True, label='IndexPen Inference Model')
-        self.indexpeninference_modelpath_layout, self.indexpeninference_modelpath_input = init_inputBox(
-            parent=self.indexpeninference_modelselection_layout,
-            label='IndexPen Model Path :',
-            default_input=config_ui.mmWave_lsl_outlet_name_default)
-        self.load_indexpen_model_btn = init_button(parent=self.indexpeninference_modelselection_layout,
-                                                   label='Load IndexPen Model')
 
         self.indexpeninference_plot_checkbox, self.indexpeninference_plot_checkbox = init_checkBox(
             parent=self.indexpeninference_control_layout, label='Inference Plot Hidden: ', default_checked=False)
@@ -94,22 +97,27 @@ class IndexPenInference(QtWidgets.QWidget):
         self.load_indexpen_model_btn.clicked.connect(self.load_indexpen_model_btn_clicked)
         self.connect_mmwave_lsl_btn.clicked.connect(self.connect_mmwave_lsl_btn_clicked)
 
-        self.interpreter_quant = None
+        self.interpreter = None
 
     def load_indexpen_model_btn_clicked(self):
-        print('load_indexpen_model_btn clicked')
-        return
+
+        print('connect_mmwave_lsl_btn clicked')
+        try:
+            self.interpreter = tf.lite.Interpreter(model_path= self.indexpeninference_modelpath_input.text())
+            self.interpreter.allocate_tensors()
+
+            # interpreter.set_tensor(input1_index, np.expand_dims(np.array(X_mmw_rD_test[0]), axis=0).astype(np.float32))
+            # interpreter.set_tensor(input2_index, np.expand_dims(np.array(X_mmw_rA_test[0]), axis=0).astype(np.float32))
+            #
+            # interpreter.invoke()
+            # predictions = interpreter.get_tensor(output_index)
+            #
+            print('Successfully load IndexPen Model: ', self.indexpeninference_modelpath_input.text())
+        except:
+            print('File does not exist')
+            raise ValueError
+
 
     def connect_mmwave_lsl_btn_clicked(self):
-        print('connect_mmwave_lsl_btn clicked')
-
-        self.interpreter_quant = tf.lite.Interpreter(model_path=str())
-        self.interpreter_quant.allocate_tensors()
-
-        # interpreter.set_tensor(input1_index, np.expand_dims(np.array(X_mmw_rD_test[0]), axis=0).astype(np.float32))
-        # interpreter.set_tensor(input2_index, np.expand_dims(np.array(X_mmw_rA_test[0]), axis=0).astype(np.float32))
-        #
-        # interpreter.invoke()
-        # predictions = interpreter.get_tensor(output_index)
-
+        print('load_indexpen_model_btn clicked')
         return
